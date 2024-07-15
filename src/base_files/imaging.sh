5badbdeedfc8efbcf6c6f44d1fc23940593b4ed9
reu24 <sophie.scc.2@gmail.com>
2024-07-15 14:40:24 -0700

    executable updates

--- src/base_files/imaging.sh
+++ src/base_files/imaging.sh
@@ -1,11 +1,5 @@
-python3 parameters.py
-
-wavelength=$1
-inclination=$2
-obs_angle=$3
-low_x=$4
-up_x=$5
-low_y=$6
-up_y=$7
-n_pixels=$8
-n_threads=$9
+python3 master.py
+python3 problem_setup.py
+radmc3d mctherm setthreads 4
+radmc3d image lambda 4.6947 incl 85 phi 0 zoomau -264 264 14 280 npix 40
+python3 generateimage.py
